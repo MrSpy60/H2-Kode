7071119da3dd3f8e08898f7946160e5463beb3f2
MrSpy60 <mrspy60@gmail.com>
2025-01-07 14:33:14 +0100

    bit shifting

--- FiveWordsFiveLetters/Program.cs
+++ FiveWordsFiveLetters/Program.cs
@@ -1,12 +1,17 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.Diagnostics;
+using System.IO;
 using System.Net.Http.Headers;
 
-string _file = "beta.txt";
+string _file = "alpha.txt";
 int _wordLength = 5;
 int _wordCount = 5;
 
 List<string> words = [];
+List<int> bitWord = [];
 
+Stopwatch sw = new Stopwatch();
+sw.Start();
 try
 {
     using (StreamReader sr = new StreamReader(_file))
@@ -25,12 +30,14 @@ try
 catch (Exception e)
 {
     Console.WriteLine(e.Message);
+    Environment.Exit(1);
 }
 Console.WriteLine("Loading done!");
 List<string> uniqueWords = [];
 //'words' contains all 5 letter words, that have no repeating letters.
 int sameLetters = 0;
 uniqueWords.Add(words[0]);
+bitWord.Add(wordtoInt(words[0]));
 foreach (string word in words)
 {
     for (int k = 0;  k < uniqueWords.Count; k++)
@@ -48,91 +55,56 @@ foreach (string word in words)
         }
         if (sameLetters == _wordLength)
         {
-            goto endOfForeach; 
+            goto endOfForeach;
         }
     }
     uniqueWords.Add(word);
+    bitWord.Add(wordtoInt(word));
     endOfForeach:;
 }
 Console.WriteLine("Anograms done!");
 // removed anograms (words containing the same letters)
+
+// 
 int _countSolutions = 0;
 Console.WriteLine(uniqueWords.Count);
-
+Console.WriteLine(bitWord.Count);
 // check for solutions
 
-recursiveFindSolution(uniqueWords, new List<string>(), uniqueWords.Count - 1);
+//recursiveFindSolution(uniqueWords, new List<string>(), uniqueWords.Count - 1);
+recursiveFindSolutionBits(bitWord, 0, 0, bitWord.Count-1,"");
+sw.Stop();
 Console.WriteLine($"{_countSolutions} Solutions");
+Console.WriteLine($"{sw.ElapsedMilliseconds } Milliseconds");
 
-void recursiveFindSolution(List<string> words , List<string> usedWords, int index)
+void recursiveFindSolutionBits(List<int> words, int usedWords, int wordscount, int index, string output)
 {
-    if (usedWords.Count == _wordCount)
+    for (int i = index ; i >= 0; i--)
     {
-        Console.WriteLine(string.Join(" ", usedWords));
-        _countSolutions++;
-        return;
-    }
-    for (int i = index; i >= 0; i--)
-    {
-        if ((string.Join("", usedWords )+ words[i]).Distinct().Count() == _wordLength * (usedWords.Count() + 1) )
+        if ((usedWords & words[i]) == 0)
         {
-            List<string> newWords = new List<string>(usedWords);
-            newWords.Add(words[i]);
-            recursiveFindSolution(words, newWords, i-1);
+            if (wordscount == _wordCount-1)
+            {
+                Console.WriteLine($"{output} {uniqueWords[i]}");
+                _countSolutions++;
+                continue;
+            }
+            recursiveFindSolutionBits(words, (usedWords | words[i]), wordscount + 1, i - 1, $"{output} {uniqueWords[i]}");
         }
     }
 }
 
-void findSolution()
+int bitPos(char c)
 {
-    int count1 = 0;
-    for (int word1 = 0; word1 < uniqueWords.Count-4 ;word1++ )
-    {
-        count1++;
-        if (count1 % 10 == 0)
-        {
-            Console.Write(".");
-        }
-        for (int word2 = word1+1; word2 < uniqueWords.Count - 3; word2++)
-        {
-            if (compareword(word1, word2))
-            {
-                for (int word3 = word2 + 1; word3 < uniqueWords.Count -2; word3++)
-                {
-                    if (compareword(word1, word3) && compareword(word2, word3))
-                    {
-                        for (int word4 = word3 + 1; word4 < uniqueWords.Count - 1; word4++)
-                        {
-                            if (compareword(word1, word4) && compareword(word2, word4) && compareword(word3, word4))
-                            {
-                                for (int word5 = word4 + 1; word5 < uniqueWords.Count; word5++)
-                                {
-                                    if (compareword(word1, word5) && compareword(word2, word5) && compareword(word3, word5) && compareword(word4, word5))
-                                    {
-                                        _countSolutions++;
-                                        Console.WriteLine(uniqueWords[word1] + " " + uniqueWords[word2] + " " + uniqueWords[word3] + " " + uniqueWords[word4] + " " + uniqueWords[word5]);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Console.WriteLine(_countSolutions);
+    return (int)(c - 'a');
 }
-bool compareword(int word1, int word2)
+
+int wordtoInt(string word)
 {
-    for (int i = 0; i < 5; i++)
+    int output = 0;
+    foreach (char c in word)
     {
-        for (int j = 0; j < 5; j++)
-        {
-            if (uniqueWords[word1][i] == uniqueWords[word2][j])
-            {
-                return false;
-            }
-        }
+        output |= 1 << bitPos(c);
     }
-    return true;
+    return output;
 }
